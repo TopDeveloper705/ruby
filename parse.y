TopDeveloper705/ruby

commit 56528da3efb32bb773b22740c24450246b861e58

Fix for bison 2.3

tool/ytab.sed
@@ -68,7 +68,7 @@ a\
   s/fprintf *(stderr,/YYFPRINTF (p,/g
 }
 s/^yysyntax_error (/&struct parser_params *p, /
-s/YYSYNTAX_ERROR yysyntax_error (/&p, /
+s/ yysyntax_error (/&p, /
 s/\( YYFPRINTF *(\)yyoutput,/\1p,/
 s/\( YYFPRINTF *(\)yyo,/\1p,/
 s/\( YYFPRINTF *(\)stderr,/\1p,/

commit 353cf6f78fc886a594b36137c8dc2927a017fec4

15f45ae4d1 and 56528da3e broke the darwin environment.
Revert "Propagate parser_params to rb_yytnamerr"

This reverts commit 15f45ae4d12f14714ab3021b60887d8c7bf4b095.

parse.y
@@ -649,8 +649,8 @@ rb_strterm_mark(VALUE obj)
 }
 #endif
 
-#define yytnamerr(yyres, yystr) (YYSIZE_T)rb_yytnamerr(p, yyres, yystr)
-size_t rb_yytnamerr(struct parser_params *p, char *yyres, const char *yystr);
+#define yytnamerr(yyres, yystr) (YYSIZE_T)rb_yytnamerr(yyres, yystr)
+size_t rb_yytnamerr(char *yyres, const char *yystr);
 
 #define TOKEN2ID(tok) ( \
     tTOKEN_LOCAL_BEGIN<(tok)&&(tok)<tTOKEN_LOCAL_END ? TOKEN2LOCALID(tok) : \
@@ -12273,9 +12273,8 @@ count_char(const char *str, int c)
  *  "\"`class' keyword\"" => "`class' keyword"
  */
 RUBY_FUNC_EXPORTED size_t
-rb_yytnamerr(struct parser_params *p, char *yyres, const char *yystr)
+rb_yytnamerr(char *yyres, const char *yystr)
 {
-    YYUSE(p);
     if (*yystr == '"') {
 	size_t yyn = 0, bquote = 0;
 	const char *yyp = yystr;

tool/ytab.sed
@@ -67,8 +67,6 @@ a\
 /^yy_reduce_print/,/^}/{
   s/fprintf *(stderr,/YYFPRINTF (p,/g
 }
-s/^yysyntax_error (/&struct parser_params *p, /
-s/YYSYNTAX_ERROR yysyntax_error (/&p, /
 s/\( YYFPRINTF *(\)yyoutput,/\1p,/
 s/\( YYFPRINTF *(\)yyo,/\1p,/
 s/\( YYFPRINTF *(\)stderr,/\1p,/